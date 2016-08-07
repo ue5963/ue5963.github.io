35d643a4ad9cbbe30ba3bca35072dca11787b205
Toshihiro Ueno <ueno@sprocket.bz>
2016-08-07 14:55:08 +0900

    version1.0.0

---
+++ README.md
@@ -0,0 +1,19 @@
+# Git Hub web page
+
+## Git Hub
+
+### ue5963 Site
+http://ue5963.github.io
+
+
+
+## 参照サイト
+
+### Ace.js
+https://ace.c9.io/#nav=about
+https://ace.c9.io/#nav=howto&api=ace
+https://ace.c9.io/build/kitchen-sink.html
+
+### Material Design
+http://materializecss.com/
+http://materializecss.com/forms.html
--- src/materialize/js/init.js
+++ src/materialize/js/init.js
@@ -1,7 +1,14 @@
+// material design code
+
 (function($){
   $(function(){
 
     $('.button-collapse').sideNav();
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+    // selsector
+    $('#selector-codeType').material_select();
+
 
   }); // end of document ready
-})(jQuery); // end of jQuery name space
+})(jQuery);
